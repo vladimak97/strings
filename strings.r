316eb52274a25475266a72eead10f6c79e37368c
Vladimiros Makris <vladimiros.makris@microsoft.wsei.edu.pl>
2023-11-08 15:06:58 +0100

    Add files via upload

---
+++ strings.r
@@ -0,0 +1,8 @@
+# Write a R program to create a list containing strings, numbers, vectors and a logical values.
+
+# Solution:
+
+list_data <- list("algorithm", "maths", c(2, 4, 6, 8), TRUE, 3.14159, 42.0)
+
+print("Data of the list:")
+print(list_data)
